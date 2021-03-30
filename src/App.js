9bdd5aef8c642fe88db2a4541d826711e3e09a33
AndyW22 <andrew.waller24@outlook.com>
2021-03-30 10:07:11 +0100

    Added api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const fetchData = async () => {
+    const data = await axios.get(
+      'https://api.openweathermap.org/data/2.5/weather?q=bristol&appid=4965de722b6db890dc8dc8c7a1829455&units=metric'
+    );
+    console.log(data.data.main.temp);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <p>Press the button below to get the weather</p>
+        <button onClick={fetchData}>Click here</button>
       </header>
     </div>
   );
