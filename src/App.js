ccff801cd0b7b575d70ae9458e413baddfb11c94
AndyW22 <andrew.waller24@outlook.com>
2021-03-30 10:11:49 +0100

    Updated weather data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import axios from 'axios';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [weatherData, setWeatherData] = React.useState(null);
+
   const fetchData = async () => {
     const data = await axios.get(
-      'https://api.openweathermap.org/data/2.5/weather?q=bristol&appid=4965de722b6db890dc8dc8c7a1829455&units=metric'
+      'https://api.openweathermap.org/data/2.5/weather?q=london&appid=4965de722b6db890dc8dc8c7a1829455&units=metric'
     );
-    console.log(data.data.main.temp);
+    setWeatherData(data.data.main.temp);
   };
   return (
     <div className='App'>
       <header className='App-header'>
         <p>Press the button below to get the weather</p>
         <button onClick={fetchData}>Click here</button>
+        {weatherData && (
+          <div>The temperatue in London is: {weatherData} degrees</div>
+        )}
       </header>
     </div>
   );
